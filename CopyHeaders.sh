d84a5f3bc6cf8af2c5aa66915d8b23b4dd1aa80b
Alsey Coleman Miller <alseycmiller@gmail.com>
2015-03-14 00:57:20 -0400

    changed variable names

--- CopyHeaders.sh
+++ CopyHeaders.sh
@@ -10,5 +10,5 @@ rm -rf ./sysroot/*
 mkdir -p sysroot/usr
 mkdir -p sysroot/usr/local
 
-scp -r $SSH_USER@$SSH_IP:/usr/include ./sysroot/usr/include
-scp -r $SSH_USER@$SSH_IP:/usr/local/include ./sysroot/usr/local/include
+scp -r $REMOTE_USER@$REMOTE_IP:/usr/include ./sysroot/usr/include
+scp -r $REMOTE_USER@$REMOTE_IP:/usr/local/include ./sysroot/usr/local/include
